e1bbbda0fead57f9b5fee7f66ae5ac219f16c31b
Anthony SKORSKI <anthony.skorski@forcity.com>
2018-08-17 16:51:08 +0200

    added code from https://alligator.io/vuejs/vue-html5-canvas/

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import MyCanvas from '@/components/MyCanvas';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'MyCanvas',
+      component: MyCanvas,
     },
   ],
 });
